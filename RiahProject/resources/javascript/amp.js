ce3f6faa98026f7a76cc7a643b88995e91f70ce4
John Foster <john44foster@aol.com>
2020-11-12 11:40:02 +0000

    Added Roah Test to Personal Website

---
+++ RiahProject/resources/javascript/amp.js
@@ -0,0 +1,20 @@
+Amplitude.init({
+    songs: [
+      {
+        name: "SadBoogie",
+        artist: "hyperlife",
+        album: "EP2",
+        url: "resources/amp/tracks/SadBoogie.mp3",
+        cover_art_url: "resources/amp/cover/hyperlife2.jpg",
+      },
+  
+      {
+        name: "Liquid Wonderment",
+        artist: "hyperlife",
+        album: "EP1",
+        url: "resources/amp/tracks/Link Up.mp3",
+        cover_art_url: "resources/amp/cover/john-smile.png",
+      }
+    ],
+    delay: 3000,
+  });
